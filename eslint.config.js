bb50608fbd2cd53c640f180b5a71b3598b7ea6d2
Børge Grunicke <boerge@grunicke.de>
2024-12-02 22:04:51 +0100

    change eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,20 @@
 export default [
 	{
 		ignores: ['build/', '.prettierrc.js', '**/.eslintrc.js', 'dist/**', 'node_modules/**'], // Replace with your ignore patterns
-		files: ['**/*.ts', '**/*.tsx'], // Specify the extensions to lint
+		files: ['**/*.ts', '**/*.tsx'],
 		languageOptions: {
+			parser: '@typescript-eslint/parser',
 			parserOptions: {
 				ecmaVersion: 'latest',
 				sourceType: 'module',
+				ecmaFeatures: {
+					jsx: true,
+				},
+				project: './tsconfig.json',
 			},
+		},
+		plugins: {
+			'@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
 		},
 		rules: {
 			// Your ESLint rules here

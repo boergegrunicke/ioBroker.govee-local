a1e4595573c982fe90b477b8ea4116561e2175d2
boergegrunicke <boerge@grunicke.de>
2025-08-01 16:43:50 +0000

    chore: passe mocha.setup.js an (chai-as-promised entfernt, nur sinon-chai aktiviert)

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -15,9 +15,7 @@ process.on('unhandledRejection', (e) => {
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 const sinonChai = require('sinon-chai');
-const chaiAsPromised = require('chai-as-promised');
 const { should, use } = require('chai');
 
 should();
 use(sinonChai);
-use(chaiAsPromised);
